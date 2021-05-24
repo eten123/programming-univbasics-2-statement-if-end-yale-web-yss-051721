e633da766671fad71e1d0ea89a4a7e3614f6723d
dashing-utility-e4d8 <eten.uket@yale.edu>
2021-05-24 14:40:05 +0000

    Automatically backed up by Learn

--- lib/if_else_end.rb
+++ lib/if_else_end.rb
@@ -1,9 +1,3 @@
 # Write your solution here
-chance_of_rain = 0.09 
-puts "Let's go outside!"
-if chance_of_rain <= 0.5
-  puts "Pack an umbrella"
-else
-  puts "Enjoy the day"
-end
-puts "Oh, and always wear sunscreen!"
+time = Time.now 
+time = time.to_i 
